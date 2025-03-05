0ee5fe9f3884d9cdd9490990866202a1511b71e4
mohitchauhan331 <mohitchauhan4793@gmail.com>
2025-03-05 11:41:50 +0530

    using arrow and function operators in js

---
+++ js/operators.js
@@ -0,0 +1,19 @@
+function sum(a, b) {
+    console.log(a + b) ;
+}
+sum(10,5);
+
+function sub(c,d){
+    console.log (c-d);
+}
+sub(15,7)
+
+var multiply = (z)=>{
+    console.log(z*5)
+}
+multiply(5);
+
+var divide =(x)=>{
+    console.log(x/5)
+}
+divide(100);
