6b2f49ddf7542d11001faa60cf5cf9bb1676c840
mohitchauhan331 <mohitchauhan4793@gmail.com>
2025-03-07 13:56:14 +0530

    practice for creating objects

---
+++ js/objects.js
@@ -0,0 +1,25 @@
+// data=["mohit",17,'hello',23.5,1];
+// console.log(data);
+
+// array of objects
+
+// category={
+//     name:"mohit",
+//     age:21,
+//     course:'react',
+//     timing:4-6,
+
+// }
+// category.fees=10000;
+// delete category.age;
+// category.course='nodejs';
+// console.log(category);
+
+stu=[
+    {name:'mohit',age:21,course:'nodejs'},
+    {name:'raghav',age:22,course:'react',timing:4},
+    {name:'ram',age:23,course:'angular',timing:6},
+]
+
+// console.log(stu)
+console.log(stu[1].course)
